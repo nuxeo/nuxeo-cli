c7f5d2e94caab519a7bf5191ebb321ed60b54b18
Arnaud Kervern <akervern@nuxeo.com>
2017-02-22 10:17:24 -0500

    NXBT-1550: Warn user about wrong Node version

--- bin/nuxeo-cli.js
+++ bin/nuxeo-cli.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 
+// Check user's node version
+const pkg = require('../package.json');
+const expectedVersion = pkg.engines.node;
+if (!require('semver').satisfies(process.version, expectedVersion)) {
+  console.log(require('log-symbols').error,
+    'Nuxeo CLI requires Node.js version ' + expectedVersion + '. ' +
+    'Current version is ' + process.version + '.');
+  process.exit(1);
+}
+
 // Notify if update is required
 const updateNotifier = require('update-notifier');
 updateNotifier({
-  pkg: require('../package.json')
+  pkg
 }).notify();
 
 const optimist = require('yargs')
