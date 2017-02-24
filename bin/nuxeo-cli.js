8af5766263e1da1e40e6bdb42ecdce8779add7d3
Arnaud Kervern <akervern@nuxeo.com>
2017-02-24 11:28:21 -0500

    NXBT-1550: Move parse logic to have minimal js code in the bin

--- bin/nuxeo-cli.js
+++ bin/nuxeo-cli.js
@@ -16,33 +16,4 @@ updateNotifier({
   pkg
 }).notify();
 
-const optimist = require('yargs')
-  .usage('Usage: $0 <command> [options] [args]')
-  .command('bootstrap', 'Bootstrap Nuxeo project, bundles or several components in the current folder.', require('../commands/bootstrap').run)
-  .example('$0 bootstrap operation listener')
-  .command('hotreload', 'Hotreload your latest modifications in a Nuxeo Server.', require('../commands/hotreload').run)
-  .example('$0 hotreload configure')
-  .command('sample', 'Generate a Sample project to discover how things work.', require('../commands/sample').run)
-  .example('$0 sample')
-  .command('update', 'Update Nuxeo CLI to get latest features.', require('../commands/update').run)
-  .options('h', {
-    alias: 'help',
-    describe: 'Print Nuxeo CLI version',
-    type: 'boolean'
-  })
-  .options('v', {
-    alias: 'version',
-    describe: 'Show version',
-    type: 'boolean'
-  });
-const argv = optimist.argv;
-
-if (argv.version) {
-  require('../lib/display_version')(console.log);
-  process.exit(0);
-}
-
-if (argv._.length === 0) {
-  optimist.showHelp();
-  process.exit(argv.help ? 0 : 1);
-}
+require('../lib/cli.js')();
