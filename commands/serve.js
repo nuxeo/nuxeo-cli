7c47b1fcfb568d1f73468436cab164b2b3c5cecc
Arnaud Kervern <arnaud.k@gmail.com>
2017-11-13 17:46:38 -0500

    WIP served files index

--- commands/serve.js
+++ commands/serve.js
@@ -4,29 +4,76 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 const mimeType = require('mime-types');
+const glob = require('glob');
+const minimatch = require('minimatch');
 
 const defaultGlob = '**/*.*';
 
-const localServer = (req, res) => {
-  // parse URL
-  const parsedUrl = url.parse(req.url, undefined, true);
-  // extract URL path
-  const pathname = `.${parsedUrl.pathname}`;
+class RouteResolver {
 
-  console.log(`${req.method} ${pathname}`);
+  constructor() {
+    this.routes = {};
+  }
 
-  fs.exists(pathname, function (exist) {
-    if (!exist) {
-      // if the file is not found, return 404
+  register(reqPath, handler) {
+    this.routes[`^.${reqPath}$`] = handler;
+  }
+
+  handle(req, res, reqPath) {
+    Object.keys(this.routes).forEach((route) => {
+      if (reqPath.match(route)) {
+        this.routes[route](req, res, reqPath);
+      }
+    });
+  }
+}
+const resolver = new RouteResolver();
+
+// Root route, in order to list all available files
+resolver.register('/', (req, res) => {
+  glob(resolver.argv.pattern, (err, files) => {
+
+    const resFiles = {};
+    files.forEach((file) => {
+      const dirname = path.format({
+        root: '/',
+        base: path.dirname(file)
+      });
+      const filename = path.basename(file);
+
+      const folder = resFiles[dirname] || {};
+      folder[filename] = `http://localhost:${resolver.argv.port}/${file}`;
+      resFiles[dirname] = folder;
+    });
+
+    res.setHeader('Content-type', 'application/json');
+    res.end(JSON.stringify(resFiles));
+  });
+});
+
+// Files serving route
+resolver.register('/.+', (req, res, pathname) => {
+  fs.exists(pathname, function (exists) {
+    debug(`Handling file: ${pathname} (${exists})`);
+    if (!exists) {
+      // if the file is not found, returns 404
       res.statusCode = 404;
       res.end(`File ${pathname} not found!`);
       return;
     }
 
-    // if is a directory returns bad request
+    // if is a directory returns 400
     if (fs.statSync(pathname).isDirectory()) {
       res.statusCode = 400;
       res.end(`${pathname} is a directory!`);
+      return;
+    }
+
+    // if the file exists but not in the glob returns 403
+    if (!minimatch(pathname, `./${resolver.argv.pattern}`)) {
+      debug(`${pathname} do not match ${resolver.argv.pattern}.`);
+      res.statusCode = 403;
+      res.end(`File ${pathname} not available.`);
       return;
     }
 
@@ -44,7 +91,7 @@ const localServer = (req, res) => {
       }
     });
   });
-};
+});
 
 module.exports = {
   command: 'serve',
@@ -53,9 +100,16 @@ module.exports = {
     require('../lib/analytics').event('nuxeo:serve', argv._.slice(1).join(' '));
     debug('Argv: %O', argv);
 
+    // Set file matching pattern (glob format)
+    resolver.argv = argv;
+
     setTimeout(() => {
-      http.createServer(localServer).listen(argv.port);
-      console.log(`Start serving local files: http://localhost:${argv.port}`);
+      http.createServer((req, res) => {
+        const pathname = `.${url.parse(req.url, undefined, true).pathname}`;
+        console.log(`${req.method} ${pathname}`);
+        resolver.handle(req, res, pathname);
+      }).listen(argv.port);
+      console.log(`Start serving files from ${process.cwd()}: http://localhost:${argv.port}`);
     }, 5);
   },
   builder: (yargs) => {
